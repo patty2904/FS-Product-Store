cbd12913f0133e953e52970c9184e816ec42b4a3
Patty <pattychr29@gmail.com>
2025-05-10 14:08:15 +0100

    created controllers for all crud ops

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,119 @@
-export const getAllProducts = async (req, res) => {
+import { sql } from "../config/db.js";
 
+//CRUD
+//C= create
+//R= read
+//U= update
+//D= delete
+
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await sql`
+        SELECT * FROM products
+        ORDER BY created_at DESC
+        `;
+
+    console.log("fetched products", products);
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error getAllProducts", error);
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 export const createProduct = async (req, res) => {
+  const { name, price, image } = req.body;
+  if (!name || !price || !image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+  try {
+    const newProduct = await sql`
+        INSERT INTO products (name, price, image)
+        VALUES (${name}, ${price}, ${image})
+        RETURNING *
+        `;
 
+    console.log("product added", newProduct[0]);
+    //201= new resource created
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.log("Error createProduct", error);
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
+export const getProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await sql`
+        SELECT * FROM products
+        WHERE id = ${id}
+        `;
+
+    console.log("product retrieved ", product);
+    res.status(200).json({ success: true, data: product[0] });
+  } catch (error) {
+    console.log("Error getProduct", error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, price, image } = req.body;
+
+  try {
+    const updatedProduct = await sql`
+        UPDATE products
+        SET name=${name}, price=${price}, image=${image}
+        WHERE id = ${id}
+        RETURNING *
+        `;
+    if (updatedProduct.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+        data: updatedProduct[0],
+      });
+    } else {
+      return res.status(201).json({ success: true, data: updatedProduct[0] });
+    }
+  } catch (error) {
+    console.log("Error updatedProduct", error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await sql`
+        DELETE FROM products
+        WHERE id = ${id}
+        RETURNING *
+        `;
+
+    console.log("product deleted ", deletedProduct[0]);
+    if (deletedProduct.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    } else {
+      return res
+        .status(200)
+        .json({
+          success: true,
+          message: "Product deleted",
+          data: deletedProduct[0],
+        });
+    }
+  } catch (error) {
+    console.log("Error deleteProduct", error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-import productRoutes from "./routes/productRoutes.js"
+import productRoutes from "./routes/productRoutes.js";
+
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
@@ -15,12 +17,32 @@ app.use(express.json());
 app.use(cors());
 //Helmet is a security middleware that helps you protect your app by setting
 //various HTTP headers
-app.use(helmet()); 
+app.use(helmet());
 //logs the requests
 app.use(morgan("dev"));
 
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
+async function initDB() {
+  try {
+    await sql`
+        CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        `;
+
+    console.log("Database initialised successfully");
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
     console.log("Server is running on port ", PORT);
-})
+  });
+});
